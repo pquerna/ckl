03ae965e7c7115eb62630fec2aaee94b20237314
Paul Querna <pquerna@apache.org>
2009-12-24 00:48:58 -0800

    bump version

--- src/ckl_version.h
+++ src/ckl_version.h
@@ -19,7 +19,7 @@
 #define _ckl_version_h_
 
 #define CKL_VERSION_MAJOR 0
-#define CKL_VERSION_MINOR 1
-#define CKL_VERSION_PATCH 3
+#define CKL_VERSION_MINOR 2
+#define CKL_VERSION_PATCH 0
 
 #endif
