906eb23da539e3d59f6dfa1516b9e3cb0fffbb17
Paul Querna <pquerna@apache.org>
2010-01-23 13:18:46 -0800

    ifdef undeed forward decls of common functions

--- extern/liboauth/src/xmalloc.h
+++ extern/liboauth/src/xmalloc.h
@@ -6,7 +6,10 @@ void *xrealloc (void *p, size_t n);
 char *xstrdup (const char *p);
 
 /* POSIX prototypes - avoid compiler warnings with '-posix' */
+#ifdef THESE_ARE_TOTALLY_NOT_NEEDED_THANKS_FROM_PAUL
 int strncasecmp(const char *s1, const char *s2, size_t n);
 int snprintf(char *str, size_t size, const char *format, ...);
 FILE *popen(const char *command, const char *type);
 int pclose(FILE *stream);
+
+#endif
